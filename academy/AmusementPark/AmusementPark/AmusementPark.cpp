8f75ebdf1814809ff9165dfff47378eeb81d030a
tht0813 <49975702+tht0813@users.noreply.github.com>
2019-04-25 19:15:30 +0900

    AmusementPark

--- academy/AmusementPark/AmusementPark/AmusementPark.cpp
+++ academy/AmusementPark/AmusementPark/AmusementPark.cpp
@@ -1,4 +1,4 @@
-﻿// ChamGame.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// AmusementPark.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include "pch.h"
@@ -6,7 +6,7 @@
 
 int main()
 {
-    std::cout << "ChamChamCham\n"; 
+    std::cout << "Amusement Park\n"; 
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
